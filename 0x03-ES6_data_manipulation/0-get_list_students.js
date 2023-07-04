7c985a0eb4c162480d3f192a3acc97ddccd1b85d
wambuiwambui <tnjoki11@gmail.com>
2023-07-04 13:19:29 +0300

    done

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+export default function getListStudents()
+{
+    return [
+        {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+        {id: 2, firstName: 'James', location: 'Columbia'},
+        {id: 5, firstname: 'Serena', location: 'San Francisco'}
+    ];
+}
+
+
+
