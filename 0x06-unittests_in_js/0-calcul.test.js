e7a212cfea5a9aef73b4b967ff908f483bd4bd31
wambuiwambui <tnjoki11@gmail.com>
2023-08-24 23:53:04 +0300

    done1

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', function () {
+    it('should return the sum of rounded numbers', function () {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+});
